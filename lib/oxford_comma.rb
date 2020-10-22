643a149c92bd85404dfbea0632cdd401f11b69e3
Artemis6425 <aurorarose6425@gmail.com>
2020-10-22 14:05:39 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    testindex = 0
+    string = ""
+    if array.length >= 3
+        while testindex < array.length-1
+            string = string+array[testindex]+", "
+            testindex +=1
+        end
+        string = string+"and "+array[testindex]
+    elsif array.length == 2
+        string = array[0]+" and "+array[1]
+    else
+        string = array[0]
+    end
+    return string
 end
